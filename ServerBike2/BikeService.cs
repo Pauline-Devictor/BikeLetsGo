abe98609729716cc0d0ccce8269d1d5c5f6b7b17
Pauline-Devictor <pauline.devictor@etu.unice.fr>
2022-11-29 13:17:49 +0100

    Trajet depart-station1 - station2 - arrivee
    Pas de prises en compte des différents contrats entre les villes (qui peut mener à l'impossibilité de poser le vélo à l'arrivée)

--- ServerBike2/BikeService.cs
+++ ServerBike2/BikeService.cs
@@ -4,8 +4,6 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Device.Location;
 using System.Text.Json;
-using System.Diagnostics.Contracts;
-using System.Runtime.Remoting.Messaging;
 
 namespace RoutingServerBike
 {
@@ -13,29 +11,33 @@ namespace RoutingServerBike
     {
 
         static readonly HttpClient client = new HttpClient();
-        public string getContracts()
-        {
-            return askContracts().Result;
-        }
+  
 
-        public string getStations(string contract)
-        {
-            return askStationsOfAContract(contract).Result.ToString();
-        }
-
-        public string getStation(string stationNumber)
-        {
-            return "ask for a station";
-        }
-        public string getStationInGivenCityCloseToUs(double latitude, double longitude, string city)
-        {
-            List<Station> stations = askStationsOfAContract(city).Result;
-            return findClosestStation(latitude, longitude, stations);
-        }
-
+        //TODO, permet de donner l'itinéraire au client
         public string getItinerary(string departure, string arrival)
         {
-            string adresses = "Depart de : " + getOSMAdress(departure).display_name + " \nArrivée à " + getOSMAdress(arrival).display_name;
+            OSMAdress depart;
+            OSMAdress arrivee;
+            try
+            {
+                depart = getOSMAdress(departure);
+                arrivee = getOSMAdress(arrival);
+            }
+            catch (Exception ex)
+            {
+                return "Addresses not found, please try again";
+            }
+
+            string closestContractToDeparture = findClosestContract(depart);
+            string closestContractToArrival = findClosestContract(arrivee);
+
+            string arret1 = findClosestStation(depart , askStationsOfAContract(closestContractToDeparture).Result);
+
+            string arret2 = findClosestStation(arrivee, askStationsOfAContract(closestContractToArrival).Result);
+
+
+            
+            string adresses = "Depart de : " + depart.display_name + " \nPrendre vélo à " + arret1 + "\nDeposer vélo à " + arret2 + " \nArrivée à " + arrivee.display_name + " ";
             return adresses;
         }
 
@@ -48,9 +50,8 @@ namespace RoutingServerBike
             //url = "https://api.jcdecaux.com/vls/v3/contracts";
             url = "https://nominatim.openstreetmap.org/search.php";
             response = JCDecauxAPICall(url, query).Result;
-            Console.WriteLine(response);
-
             List<OSMAdress> adresses = JsonSerializer.Deserialize<List<OSMAdress>>(response);
+            
             if (adresses != null)
             {
                return adresses[0];
@@ -60,14 +61,15 @@ namespace RoutingServerBike
         }
 
 
-
-        public async Task<string> askContracts()
+        public async Task<List<JCDContract>> askContracts()
         {
             HttpResponseMessage response = await client.GetAsync("https://api.jcdecaux.com/vls/v1/contracts?apiKey=07a2f22eaa786639241a704c091d22c6875b5809");
             response.EnsureSuccessStatusCode();
             string responseBody = await response.Content.ReadAsStringAsync();
-            Console.WriteLine(responseBody);
-            return responseBody;
+            List<JCDContract> contracts = JsonSerializer.Deserialize<List<JCDContract>>(responseBody);
+
+            contracts = cleanContractList(contracts);
+            return contracts;
         }
 
         public async Task<List<Station>> askStationsOfAContract(string contract)
@@ -79,13 +81,12 @@ namespace RoutingServerBike
             return stations;
         }
 
-        public string findClosestStation(double latitude, double longitude, List<Station> stations)
+        public string findClosestStation(OSMAdress address, List<Station> stations)
         {
-            GeoCoordinate clientPosition = new GeoCoordinate(latitude, longitude);
+            GeoCoordinate clientPosition = new GeoCoordinate(changeToDouble(address.lat), changeToDouble(address.lon));
 
             double savedDistance = -1;
             Station currentClosestStation = null;
-            GeoCoordinate chosenStationGo = new GeoCoordinate(latitude, longitude);
 
             foreach (Station station in stations)
             {
@@ -100,9 +101,6 @@ namespace RoutingServerBike
             return currentClosestStation.name;
         }
 
-
-
-
         static async Task<string> JCDecauxAPICall(string url, string query)
         {
             //HttpResponseMessage response = await client.GetAsync(url + "?" + query);
@@ -112,22 +110,50 @@ namespace RoutingServerBike
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsStringAsync();
         }
+
+
+        
+        //Retourne la ville la plus proche des coordonnées données
+        public string findClosestContract(OSMAdress position)
+        {
+            double positionLat = changeToDouble(position.lat);
+            double positionLon = changeToDouble(position.lon);
+            GeoCoordinate coordIndication = new GeoCoordinate(positionLat, positionLon);
+            List<JCDContract> contracts = askContracts().Result;
+   
+            double savedDistance = -1;
+            String currentClosestContract = null;
+            GeoCoordinate currentPosition = new GeoCoordinate(changeToDouble(position.lat), changeToDouble(position.lon));
+
+            foreach (JCDContract contract in contracts)
+            {
+                OSMAdress currentContract = getOSMAdress(contract.name);
+
+                GeoCoordinate currentstationGo = new GeoCoordinate(changeToDouble(currentContract.lat), changeToDouble(currentContract.lon));
+                double distance = coordIndication.GetDistanceTo(currentstationGo);
+                if (distance != 0 && (savedDistance == -1 || distance < savedDistance))
+                {
+                    savedDistance = distance;
+                    currentClosestContract = contract.name;
+                }
+            }
+            return currentClosestContract;
+
+        }
+
+        public double changeToDouble(string value)
+        {
+            value = value.Replace(".", ",");
+            return Convert.ToDouble(value);
+        }
+
+
+        public List<JCDContract> cleanContractList(List<JCDContract> contracts)
+        {
+            contracts.RemoveAll(x => x.cities == null); // suppression dans la liste des contracts qui ne sont pas des villes 
+            return contracts;
+        }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     public class Station
@@ -147,6 +173,15 @@ namespace RoutingServerBike
     public class JCDContract
     {
         public string name { get; set; }
+        public string commercial_name { get; set; }
+        public List<string> cities { get; set; }
+        public string country_code { get; set; }
+
+        public override string ToString()
+        {
+            return
+                $"{nameof(name)}: {name}, {nameof(commercial_name)}: {commercial_name}, {nameof(cities)}: {cities}, {nameof(country_code)}: {country_code}";
+        }
     }
 
     public class JCDStation
@@ -162,6 +197,8 @@ namespace RoutingServerBike
         public string lon { get; set; }
         public string display_name { get; set; }
     }
+
+
 
 
 }
--- ServerBike2/IBikeService.cs
+++ ServerBike2/IBikeService.cs
@@ -7,17 +7,6 @@ namespace RoutingServerBike
     public interface IBikeService
     {
         [OperationContract]
-        //Récupère tous les contrats
-        string getContracts();
-
-        [OperationContract]
-        string getStations(string contract);
-        [OperationContract]
-        string getStation(string station);
-        [OperationContract]
-        string getStationInGivenCityCloseToUs(double latitude, double longitude, string city);
-
-        [OperationContract]
         string getItinerary(string departure, string arrival);
     }
 
